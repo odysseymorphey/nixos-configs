54ce0e08e1a36aa7666e15c5f4b9dddd1d8e68fd
fredacha <ramazanov-zalim@mail.ru>
2025-11-08 18:08:30 +0300

    .

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
 
         home-manager.nixosModules.home-manager
         {
+          home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.nixos = import ./home/nixos/home.nix;
         }
